68b0dc0d16b9abba56d10da2034da268ccc6ac40
MXuDong <2941884109@qq.com>
2019-12-30 11:44:53 +0800

    add the replacer file

---
+++ cores/replacer.py
@@ -0,0 +1,23 @@
+"""
+Replacer in the cores package, it provide some method and class to deal the string of args or values.
+
+:author: MXuDong
+"""
+
+
+class Replacer:
+    """
+    Replacer provides some method for the string replace
+
+    :author: MXuDong
+    """
+
+    def __init__(self, target_value="", args={}):
+        """
+        the init method of the Replacer
+
+        :param target_value: the target_value is will be replace string value
+        :param args: the args is the value of replace
+        """
+        self._target_value = target_value
+        self._args = args
