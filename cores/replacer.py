1e50f0968f2a9a492259001397a30dbd6fbd20fd
MXuDong <2941884109@qq.com>
2019-12-30 23:35:01 +0800

    add the init method for inner class

--- cores/replacer.py
+++ cores/replacer.py
@@ -19,6 +19,23 @@ class Replacer:
         It is an iterator of the string param value
         """
 
+        def __init__(self, target_value, flag_char='$', param_flag='@', value_flag='^'):
+            """
+            init the VarIterator, and set some chars
+
+            :param flag_char: the string param flag, default is $
+            :param param_flag: the params' flag, default is @, the format is @param_name=param_value
+            :param value_flag: the values' flag, default is ^, the format is ^value1 value2 or other^
+            """
+            self.__index = 0
+            self.__open_index = -1
+            self.__close_index = -1
+            self.__flag_char = flag_char
+            self.__param_flag = param_flag
+            self.__value_flag = value_flag
+            self.__target_value = target_value
+            pass
+
     def __init__(self, target_value="", args={}):
         """
         the init method of the Replacer
