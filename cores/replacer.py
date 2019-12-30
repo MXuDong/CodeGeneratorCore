384af89caa1613e802e39f57d4dc3f54ea5ba61c
MXuDong <2941884109@qq.com>
2019-12-30 23:49:50 +0800

    add func's document

--- cores/replacer.py
+++ cores/replacer.py
@@ -19,13 +19,19 @@ class Replacer:
         It is an iterator of the string param value
         """
 
-        def __init__(self, target_value, flag_char='$', param_flag='@', value_flag='^'):
+        def __init__(self, target_value, flag_char='$', param_flag='@', value_flag='^', convert_flag="&"):
             """
             init the VarIterator, and set some chars
 
+            and for the flag must be different, else will has some error for convert_flag
+
+            :param target_value: the target value, which will be replace
             :param flag_char: the string param flag, default is $
             :param param_flag: the params' flag, default is @, the format is @param_name=param_value
             :param value_flag: the values' flag, default is ^, the format is ^value1 value2 or other^
+            :param convert_flag: the values' flag, default is &, for the
+                flag_char, param_flag, value_flag, convert_flag convert_flag will be turn to itself by
+                convert_flag
             """
             self.__index = 0
             self.__open_index = -1
@@ -33,8 +39,14 @@ class Replacer:
             self.__flag_char = flag_char
             self.__param_flag = param_flag
             self.__value_flag = value_flag
+            self.__convert_flag = convert_flag
             self.__target_value = target_value
-            pass
+
+        def has_next(self):
+            """
+            if the target value has next param, it will return true, else return false
+            :return: boolean
+            """
 
     def __init__(self, target_value="", args={}):
         """
