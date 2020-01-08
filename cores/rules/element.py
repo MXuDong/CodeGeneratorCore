76db25c105f146919f35e8c9e45f72bf1bae39d0
MXuDong <2941884109@qq.com>
2020-01-08 14:52:45 +0800

    core replace complete, start new core: parser

---
+++ cores/rules/element.py
@@ -0,0 +1,14 @@
+"""
+The doc tree, use this to convert. All the yaml will be convert to this format.
+"""
+
+
+class Element:
+    """
+    all value will be converted to this class.
+    """
+
+    def __init__(self):
+        self._name = ""
+        self._value = None
+        self.type = None
---
+++ cores/rules/rule_config.py
@@ -0,0 +1,9 @@
+"""
+The element build config, the element will use this config to build the Elements.
+"""
+
+
+class RuleConfig:
+    """
+    Config the rules, it provide some method to build.
+    """
